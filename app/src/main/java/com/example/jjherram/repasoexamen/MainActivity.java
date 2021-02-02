1037ffc9fcd15d9ec8db6242d11bc5f1e7b877b2
Quiqueferre12 <quiqueferre12@gmail.com>
2021-02-02 16:30:13 +0100

    esto

--- app/src/main/java/com/example/jjherram/repasoexamen/MainActivity.java
+++ app/src/main/java/com/example/jjherram/repasoexamen/MainActivity.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     //ANGLE PRESIDENTE
     //Añañin
+    //tab layout
+    //2 actividades
+    //recycler view con info de los pojos
+    //pulsar un boton lanzar actividad en primer plano
+    //pedir permisos
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
