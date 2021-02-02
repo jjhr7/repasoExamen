b50976fb64642f4325bc8f2e9dbbe324d8b41a7f
Jose Julio <jjpeajar@epsg.upv.es>
2021-02-02 15:53:27 +0100

    putón

--- app/src/main/java/com/example/jjherram/repasoexamen/MainActivity.java
+++ app/src/main/java/com/example/jjherram/repasoexamen/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //ANGLE PRESIDENTE
+    //Añañin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
