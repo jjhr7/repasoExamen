4b7eedb59d88414821d57dad8ae89dc5bb633ef3
Andrey <andreykuzmin342@gmail.com>
2021-02-02 17:00:30 +0100

    Tabs

--- app/src/main/java/com/example/jjherram/repasoexamen/ExamenAngle/MainActivity.java
+++ app/src/main/java/com/example/jjherram/repasoexamen/ExamenAngle/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.jjherram.repasoexamen.ExamenAngle;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.os.Bundle;
 
+
 import com.example.jjherram.repasoexamen.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
     }
+
 }
